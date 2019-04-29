a2e5bfdadea5b11504389b3b1b287a903a0a8d66
Alex Lee <hyunho98@gmail.com>
2019-04-29 00:36:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var obj = object;
+  var obj = Object.assign({}, object);
   delete obj[key];
   return obj;
 }
