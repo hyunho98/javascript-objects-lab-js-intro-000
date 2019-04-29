90b7f2e88554c4e312e98a805c886957be5f6081
Alex Lee <hyunho98@gmail.com>
2019-04-29 00:17:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
   var obj = Object.assign({}, object, { [key]: value });
   return obj;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
